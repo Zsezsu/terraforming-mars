5ac539038a794a261aefad9e162823dad93e06ee
sagi-viktor <sagi.viktor1@gmail.com>
2022-03-02 20:53:43 +0100

    Add skeleton for login and registration related functions in helper.py

--- helper.py
+++ helper.py
@@ -11,6 +11,50 @@ def verify_password(plain_text_password, hashed_password):
 
 
 def update_password_on_form(form, password):
+    """Replaces the unhashed password withhashed one
+    in dictionary"""
     form = dict(form)
     form['password'] = password
     return form
+
+
+def validate_registration(form):
+    """If every input matches the requirements
+    returns True"""
+    pass
+
+
+def validate_login(form):
+    """If every input matches the requirements
+    returns True"""
+    pass
+
+
+def submit_registration(form):
+    """This submits !valid! data to database
+    If process successful returns user id"""
+    return user_id
+
+
+def is_password_format_correct(password):
+    """If password formatted like, 'format_template'
+    returns True"""
+    return bool
+
+
+def is_username_exist(username):
+    """If username already exist in database
+    returns True"""
+    return bool
+
+
+def unpack_registration_form(form_data):
+    """Unpacks registration from coming from html
+    returns username, email, password, pp_id = (profile_picture_id)"""
+    return [username, email, password, pp_id]
+
+
+def unpack_login_form(form):
+    """Unpacks login form coming from html
+    returns login_token, password"""
+    return [login_token, password]
