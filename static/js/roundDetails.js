400dce25c4068042458372a8eb0a01e96f4d8d60
Zsezsu <zsezsu6@gmail.com>
2022-03-03 19:10:19 +0100

    Add pageControl startings to round details

--- static/js/roundDetails.js
+++ static/js/roundDetails.js
@@ -3,11 +3,38 @@ const roundDetails = {
         const id = this.getDataValue('id');
         const roundStatus = this.getDataValue('status');
         console.log(roundStatus)
+        this.pageControl(roundStatus)
     },
 
     getDataValue(dataValue){
         const containerDiv = document.querySelector('.container');
         return containerDiv.getAttribute(`data-${dataValue}`);
+    },
+
+    pageControl(roundStatus){
+        const containerDiv = document.querySelector('.container');
+        if (roundStatus === 'init_round'){
+             containerDiv.insertAdjacentHTML('afterbegin', `<button id="init">Init round</button>`);
+             let button = document.querySelector('#init');
+             button.addEventListener('click', initButtonClick)
+        } else if (roundStatus === 'started'){
+
+        } else if (roundStatus === 'finished'){
+
+        }
+
+        function initButtonClick(event){
+            containerDiv.insertAdjacentHTML(
+                "beforeend",
+                `<label for="table" class="text-light">Please select the table of this game</label>
+                    <select>
+                        <option></option>
+                    </select>`)
+        }
+    },
+
+    async fetchData(){
+        let response = await fetch('/api/boards');
     },
 
 
