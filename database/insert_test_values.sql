fcfb73d677eb0e1220c3f492340fe177bf24fced
benedekhalaj <bendihalaj@gmail.com>
2022-03-03 12:47:39 +0100

    Add test values for leagues and rounds

--- database/insert_test_values.sql
+++ database/insert_test_values.sql
@@ -7,3 +7,16 @@ VALUES
     ('Viktor', 'viktor@mars.hu', '1234'),
     ('Akárki', 'akarki@mars.hu', '1234');
 
+INSERT INTO
+    leagues(round_number)
+VALUES
+    (5);
+
+INSERT INTO
+    rounds(league_id, sequence)
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (1, 5);
