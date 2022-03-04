7d1cc2086b61c5961bc262f6f0f63f84a1128218
dudaskobende <dudaskobende@gmail.com>
2022-03-04 14:33:17 +0100

    Add round image

--- database/insert_images.sql
+++ database/insert_images.sql
@@ -9,6 +9,8 @@ VALUES
     ('img/league_cards/mars-4.webp', TRUE),
     ('img/league_cards/mars-5.webp', TRUE);
 
+------------------------------------insert league images --------------------------------
+
 INSERT INTO
     images(source, round_card)
-VALUES ('img/league_cards/mars-1.webp', TRUE);
+VALUES ('img/round_cards/mars-locked.webp', TRUE);
