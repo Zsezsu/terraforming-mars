d107f44209ee9633987bdeefb0d62e694c3f100e
benedekhalaj <bendihalaj@gmail.com>
2022-03-04 21:49:25 +0100

    Add inputs for editable table cells in started phase

--- static/js/roundStarted.js
+++ static/js/roundStarted.js
@@ -1,1 +1,14 @@
-console.log('connected');
+// const editableTableData = document.querySelectorAll('td.editable');
+// editableTableData.forEach(data => data.addEventListener('click', editTableData));
+//
+// function editTableData(clickEvent) {
+//     const data = clickEvent.target;
+//     const inputField = createInputField();
+//     data.innerHTML = inputField;
+// }
+//
+// function createInputField() {
+//     return `
+//     <input type="number" placeholder="edit" class="edit-column">
+//     `
+// }
