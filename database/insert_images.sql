fb0ec9c75d83c537be9e415075e77d5ed120a559
dudaskobende <dudaskobende@gmail.com>
2022-03-04 14:40:15 +0100

    Add insert images

--- database/insert_images.sql
+++ database/insert_images.sql
@@ -7,32 +7,10 @@ VALUES
     ('img/league_cards/mars-2.webp', TRUE),
     ('img/league_cards/mars-3.webp', TRUE),
     ('img/league_cards/mars-4.webp', TRUE),
-    ('img/league_cards/mars-5.webp', TRUE),
-    ('img/league_cards/mars-6.webp', TRUE),
-    ('img/league_cards/mars-7.webp', TRUE),
-    ('img/league_cards/mars-8.webp', TRUE);
+    ('img/league_cards/mars-5.webp', TRUE);
 
-
-------------------------------------insert player images --------------------------------
-
+------------------------------------insert league images --------------------------------
 
 INSERT INTO
-    images(source, user_image)
-VALUES
-    ('img/player_icons/player-card-1.webp', TRUE),
-    ('img/player_icons/player-card-2.webp', TRUE),
-    ('img/player_icons/player-card-3.webp', TRUE),
-    ('img/player_icons/player-card-4.webp', TRUE),
-    ('img/player_icons/player-card-5.webp', TRUE),
-    ('img/player_icons/player-card-6.webp', TRUE),
-    ('img/player_icons/player-card-7.webp', TRUE),
-    ('img/player_icons/player-card-8.webp', TRUE),
-    ('img/player_icons/player-card-9.webp', TRUE),
-    ('img/player_icons/player-card-10.webp', TRUE),
-    ('img/player_icons/player-card-11.webp', TRUE),
-    ('img/player_icons/player-card-12.webp', TRUE),
-    ('img/player_icons/player-card-13.webp', TRUE),
-    ('img/player_icons/player-card-14.webp', TRUE),
-    ('img/player_icons/player-card-15.webp', TRUE),
-    ('img/player_icons/player-card-16.webp', TRUE),
-    ('img/player_icons/player-card-17.webp', TRUE);
+    images(source, round_card)
+VALUES ('img/round_cards/mars-locked.webp', TRUE);
