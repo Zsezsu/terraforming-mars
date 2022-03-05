ee4813888f4c95f2f0b41cadec13e01d18ba9e4f
dudaskobende <dudaskobende@gmail.com>
2022-03-05 12:04:47 +0100

    Add datahandler to javascript

---
+++ static/js/data/dataHandler.js
@@ -0,0 +1,41 @@
+export const dataHandler = {
+    //GET
+    getPlayers: async function () {
+        return await apiGet(`/api/players`);
+    },
+    getLoggedInUser: async function () {
+        return await apiGet(`/api/users/logged-in`);
+    },
+    getImages: async function(imageType) {
+        return await apiGet(`/api/images/${imageType}`);
+    },
+    getBoards: async function() {
+        return await apiGet(`/api/boards`);
+    },
+
+    //POST
+    postNewLeague: async function (data) {
+        return await apiPost(`/api/leagues`, data);
+    }
+}
+
+async function apiGet(url) {
+    let response = await fetch(url, {method: 'GET'});
+    if (response.ok) {
+        return await response.json();
+    }
+}
+
+async function apiPost(url, payload) {
+    const response = await fetch(
+        url,
+        {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(payload)
+        }
+    );
+    if (response.ok) {
+        return await response.json();
+    }
+}
