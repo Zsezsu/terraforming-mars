16c41e7607ad3aad8fc8a95583a840fd9d1d5865
sagi-viktor <sagi.viktor1@gmail.com>
2022-03-07 18:22:00 +0100

    Add restrictions for non logged in users

--- server.py
+++ server.py
@@ -32,7 +32,10 @@ def design():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    if session['UID']:
+        return render_template('dashboard.html')
+    else:
+        return redirect(url_for('registration'))
 
 
 @app.route('/profile')
@@ -52,10 +55,13 @@ def league(league_id):
 
 @app.route('/my-leagues')
 def leagues():
-    uid = session['UID']
-    logged_in_user = select_queries.get_logged_in_user(uid)
-    logged_in_user_leagues = select_queries.get_logged_in_user_leagues(uid)
-    return render_template('my_leagues.html', logged_in_user=logged_in_user, leagues=logged_in_user_leagues)
+    if session['UID']:
+        uid = session['UID']
+        logged_in_user = select_queries.get_logged_in_user(uid)
+        logged_in_user_leagues = select_queries.get_logged_in_user_leagues(uid)
+        return render_template('my_leagues.html', logged_in_user=logged_in_user, leagues=logged_in_user_leagues)
+    else:
+        return redirect(url_for('registration'))
 
 
 @app.route('/account/signup')
@@ -101,40 +107,46 @@ def logout():
 
 @app.route('/league/<league_id>/round/<round_id>', methods=['GET'])
 def results(league_id=1, round_id=2):
-    round_data = select_queries.get_round_by_id(round_id)
-    round_status = helper.get_round_status(round_data)
-    game_data, players_data, players_in_game, round_points = (None, None, None, None)
+    if session['UID']:
+        round_data = select_queries.get_round_by_id(round_id)
+        round_status = helper.get_round_status(round_data)
+        game_data, players_data, players_in_game, round_points = (None, None, None, None)
 
-    if round_status == 'init_round':
-        players_data = select_queries.get_round_players(league_id)
-        game_data = {
-            "boards": select_queries.get_boards(),
-            "expansions": select_queries.get_expansions(),
-            "corporations": select_queries.get_corporations()
-        }
+        if round_status == 'init_round':
+            players_data = select_queries.get_round_players(league_id)
+            game_data = {
+                "boards": select_queries.get_boards(),
+                "expansions": select_queries.get_expansions(),
+                "corporations": select_queries.get_corporations()
+            }
 
-    elif round_status == 'started':
-        players_in_game = select_queries.get_players_in_round(round_id)
-    elif round_status == 'finished':
-        round_points = select_queries.get_round_points(round_id)
+        elif round_status == 'started':
+            players_in_game = select_queries.get_players_in_round(round_id)
+        elif round_status == 'finished':
+            round_points = select_queries.get_round_points(round_id)
+        else:
+            return redirect('/')
+        return render_template('round_details.html',
+                               round_status=round_status,
+                               round_points=round_points,
+                               round=round_data,
+                               round_id=round_id,
+                               league_id=league_id,
+                               game=game_data,
+                               players=players_data,
+                               players_in_game=players_in_game)
     else:
-        return redirect('/')
-    return render_template('round_details.html',
-                           round_status=round_status,
-                           round_points=round_points,
-                           round=round_data,
-                           round_id=round_id,
-                           league_id=league_id,
-                           game=game_data,
-                           players=players_data,
-                           players_in_game=players_in_game)
+        return redirect(url_for('registration'))
 
 
 @app.route('/league/<league_id>/round/<round_id>', methods=['POST'])
 def init_round(league_id, round_id):
-    round_details = request.form
-    insert_queries.init_round(round_details, round_id)
-    return redirect(f'/league/{league_id}/round/{round_id}')
+    if session['UID']:
+        round_details = request.form
+        insert_queries.init_round(round_details, round_id)
+        return redirect(f'/league/{league_id}/round/{round_id}')
+    else:
+        return redirect(url_for('registration'))
 
 
 if __name__ == '__main__':
