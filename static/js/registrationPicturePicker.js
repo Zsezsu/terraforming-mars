494ba076cefbbe52c7ede44cf18175b7ee22311e
benedekhalaj <bendihalaj@gmail.com>
2022-03-09 17:41:37 +0100

    Rework registration picture picker

--- static/js/registrationPicturePicker.js
+++ static/js/registrationPicturePicker.js
@@ -1,20 +1,13 @@
-
 function initClick() {
-  let pictures = document.querySelectorAll('img.profile-picture');
-  pictures.forEach(e => {
-    e.addEventListener('click', getPictureId);
-  })
+  const submitButton = document.querySelector('#submit-btn');
+  submitButton.addEventListener('click', getPictureId);
 }
 
-function getPictureId(e) {
-  const id = e.currentTarget.id;
-  document.querySelector('#picture-input').value = id;
-  document.querySelector('#hidden-button').removeAttribute('hidden');
-  let allPicture = e.currentTarget.parentElement.children;
-  for (let picture of allPicture) {
-    picture.classList.remove('selected');
-  }
-  e.currentTarget.classList.add('selected');
+function getPictureId() {
+  const picture = document.querySelector('#slideshow-profile');
+  const pictureId = picture.getAttribute('data-image-id');
+  const pictureInput = document.querySelector('#picture-input');
+  pictureInput.value = pictureId;
 }
 
 initClick()
