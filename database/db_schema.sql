f29f93568e517aabdb7e66a18f6ea2187761b429
dudaskobende <dudaskobende@gmail.com>
2022-03-10 14:48:03 +0100

    Refactor query keys

--- database/db_schema.sql
+++ database/db_schema.sql
@@ -4,20 +4,12 @@
 
 -- Database version = 13.6
 
----------------------------------------------Drop primary keys-----------------------------------------------------
-
-ALTER TABLE IF EXISTS ONLY public.corporations          DROP CONSTRAINT IF EXISTS corporations_pkey     CASCADE;
-ALTER TABLE IF EXISTS ONLY public.boards                DROP CONSTRAINT IF EXISTS boards_pkey           CASCADE;
-ALTER TABLE IF EXISTS ONLY public.expansions            DROP CONSTRAINT IF EXISTS expansions_pkey       CASCADE;
-ALTER TABLE IF EXISTS ONLY public.leagues               DROP CONSTRAINT IF EXISTS leagues_pkey          CASCADE;
-ALTER TABLE IF EXISTS ONLY public.results               DROP CONSTRAINT IF EXISTS results_pkey          CASCADE;
-ALTER TABLE IF EXISTS ONLY public.players               DROP CONSTRAINT IF EXISTS players_pkey          CASCADE;
-
 ---------------------------------------------Drop foreign keys-----------------------------------------------------
 
 ALTER TABLE IF EXISTS ONLY public.milestones            DROP CONSTRAINT IF EXISTS fk_board_id           CASCADE;
 ALTER TABLE IF EXISTS ONLY public.milestones            DROP CONSTRAINT IF EXISTS fk_expansion_id       CASCADE;
 ALTER TABLE IF EXISTS ONLY public.awards                DROP CONSTRAINT IF EXISTS fk_board_id           CASCADE;
+ALTER TABLE IF EXISTS ONLY public.awards                DROP CONSTRAINT IF EXISTS fk_expansion_id       CASCADE;
 ALTER TABLE IF EXISTS ONLY public.league_players        DROP CONSTRAINT IF EXISTS fk_league_id          CASCADE;
 ALTER TABLE IF EXISTS ONLY public.rounds                DROP CONSTRAINT IF EXISTS fk_league_id          CASCADE;
 ALTER TABLE IF EXISTS ONLY public.round_players         DROP CONSTRAINT IF EXISTS fk_round_id           CASCADE;
@@ -26,75 +18,75 @@ ALTER TABLE IF EXISTS ONLY public.game_setup            DROP CONSTRAINT IF EXIST
 ALTER TABLE IF EXISTS ONLY public.game_setup            DROP CONSTRAINT IF EXISTS fk_board_id           CASCADE;
 ALTER TABLE IF EXISTS ONLY public.game_setup            DROP CONSTRAINT IF EXISTS fk_expansion_id       CASCADE;
 ALTER TABLE IF EXISTS ONLY public.points                DROP CONSTRAINT IF EXISTS fk_round_id           CASCADE;
+ALTER TABLE IF EXISTS ONLY public.points                DROP CONSTRAINT IF EXISTS fk_player_id          CASCADE;
+ALTER TABLE IF EXISTS ONLY public.corporations          DROP CONSTRAINT IF EXISTS fk_expansion_id       CASCADE;
 
+---------------------------------------------Drop primary keys-----------------------------------------------------
+
+ALTER TABLE IF EXISTS ONLY public.images          DROP CONSTRAINT IF EXISTS pk_images_id;
+ALTER TABLE IF EXISTS ONLY public.corporations          DROP CONSTRAINT IF EXISTS pk_corporations_id;
+ALTER TABLE IF EXISTS ONLY public.boards          DROP CONSTRAINT IF EXISTS pk_boards_id;
+ALTER TABLE IF EXISTS ONLY public.expansions          DROP CONSTRAINT IF EXISTS pk_expansions_id;
+ALTER TABLE IF EXISTS ONLY public.milestones          DROP CONSTRAINT IF EXISTS pk_milestones_id;
+ALTER TABLE IF EXISTS ONLY public.awards          DROP CONSTRAINT IF EXISTS pk_awards_id;
+ALTER TABLE IF EXISTS ONLY public.players          DROP CONSTRAINT IF EXISTS pk_players_id;
+ALTER TABLE IF EXISTS ONLY public.leagues          DROP CONSTRAINT IF EXISTS pk_leagues_id;
+ALTER TABLE IF EXISTS ONLY public.league_players          DROP CONSTRAINT IF EXISTS pk_league_players;
+ALTER TABLE IF EXISTS ONLY public.rounds          DROP CONSTRAINT IF EXISTS pk_rounds_id;
+ALTER TABLE IF EXISTS ONLY public.round_players          DROP CONSTRAINT IF EXISTS pk_round_players_id;
+ALTER TABLE IF EXISTS ONLY public.game_setup          DROP CONSTRAINT IF EXISTS pk_game_setup_id;
+ALTER TABLE IF EXISTS ONLY public.points          DROP CONSTRAINT IF EXISTS pk_points_id;
 
 --------------------------------------------------Game Properties--------------------------------------------------
 
 DROP TABLE IF EXISTS images;
 CREATE TABLE images
 (
-    id          SERIAL UNIQUE NOT NULL,
-    source       TEXT,
+    id          SERIAL,
+    source      TEXT,
     league_card BOOLEAN DEFAULT FALSE,
     round_card  BOOLEAN DEFAULT FALSE,
-    user_image  BOOLEAN DEFAULT FALSE,
-    corporation BOOLEAN DEFAULT FALSE
+    user_image  BOOLEAN DEFAULT FALSE
 );
 
 DROP TABLE IF EXISTS corporations;
 CREATE TABLE corporations
 (
-    id              SERIAL UNIQUE NOT NULL,
+    id              SERIAL,
     name            TEXT,
-    expansion_id    INTEGER,
-    image_id        INTEGER,
-    PRIMARY KEY (id)
+    expansion_id    INTEGER
 );
 
 DROP TABLE IF EXISTS boards;
 CREATE TABLE boards
 (
-    id         SERIAL UNIQUE NOT NULL,
-    board_name TEXT,
-    PRIMARY KEY (id)
+    id         SERIAL,
+    board_name TEXT
 );
 
 DROP TABLE IF EXISTS expansions;
 CREATE TABLE expansions
 (
-    id             SERIAL UNIQUE NOT NULL,
-    expansion_name TEXT          NOT NULL,
-    PRIMARY KEY (id)
+    id             SERIAL,
+    expansion_name TEXT          NOT NULL
 );
 
 DROP TABLE IF EXISTS milestones;
 CREATE TABLE milestones
 (
-    id             SERIAL UNIQUE NOT NULL,
+    id             SERIAL,
     milestone_name TEXT          NOT NULL,
     board_id       INTEGER,
-    expansion_id   INTEGER,
-    CONSTRAINT fk_board_id
-        FOREIGN KEY (board_id)
-            REFERENCES boards (id) ON DELETE CASCADE,
-    CONSTRAINT fk_expansion_id
-        FOREIGN KEY (expansion_id)
-            REFERENCES expansions (id)
+    expansion_id   INTEGER
 );
 
 DROP TABLE IF EXISTS awards;
 CREATE TABLE awards
 (
-    id           SERIAL UNIQUE NOT NULL,
+    id           SERIAL,
     awards_name  TEXT          NOT NULL,
     board_id     INTEGER,
-    expansion_id INTEGER,
-    CONSTRAINT fk_board_id
-        FOREIGN KEY (board_id)
-            REFERENCES boards (id) ON DELETE CASCADE,
-    CONSTRAINT fk_expansion_id
-        FOREIGN KEY (expansion_id)
-            REFERENCES expansions (id)
+    expansion_id INTEGER
 );
 
 --------------------------------------------------Create Tables--------------------------------------------------
@@ -104,89 +96,66 @@ CREATE TABLE awards
 DROP TABLE IF EXISTS players;
 CREATE TABLE players
 (
-    id          SERIAL UNIQUE  NOT NULL,
+    id          SERIAL,
     username    TEXT UNIQUE    NOT NULL,
     first_name  TEXT           NOT NULL,
     last_name   TEXT           NOT NULL,
     email       VARCHAR UNIQUE NOT NULL,
     password    VARCHAR        NOT NULL,
-    image_id    INTEGER,
-    PRIMARY KEY (id)
+    image_id    INTEGER
 );
 
 DROP TABLE IF EXISTS leagues;
 CREATE TABLE leagues
 (
-    id              SERIAL UNIQUE NOT NULL,
+    id              SERIAL,
     league_name     TEXT,
     league_admin    INTEGER       NULL,
     round_number    INTEGER,
-    image_id        INTEGER,
-    PRIMARY KEY (id)
+    image_id        INTEGER
 );
 
 DROP TABLE IF EXISTS league_players;
 CREATE TABLE league_players
 (
-    id        SERIAL UNIQUE,
+    id        SERIAL,
     league_id INTEGER,
-    player_id INTEGER,
-    CONSTRAINT fk_league_id
-        FOREIGN KEY (league_id)
-            REFERENCES leagues (id) ON DELETE CASCADE
+    player_id INTEGER
 );
 
 DROP TABLE IF EXISTS rounds;
 CREATE TABLE rounds
 (
-    id          SERIAL UNIQUE NOT NULL,
+    id          SERIAL,
     league_id   INTEGER,
     sequence    INTEGER,
     started     BOOLEAN DEFAULT FALSE,
     finished    BOOLEAN DEFAULT FALSE,
-    image_id    INTEGER,
-    CONSTRAINT fk_league_id
-        FOREIGN KEY (league_id)
-            REFERENCES leagues (id) ON DELETE CASCADE
+    image_id    INTEGER
 );
 
 DROP TABLE IF EXISTS round_players;
 CREATE TABLE round_players
 (
-    id             SERIAL UNIQUE NOT NULL,
+    id             SERIAL,
     round_id       INTEGER,
     player_id      INTEGER,
-    corporation_id INTEGER,
-    CONSTRAINT fk_round_id
-        FOREIGN KEY (round_id)
-            REFERENCES rounds (id) ON DELETE CASCADE,
-    CONSTRAINT fk_corporation_id
-        FOREIGN KEY (corporation_id)
-            REFERENCES corporations (id) ON DELETE CASCADE
+    corporation_id INTEGER
 );
 
 DROP TABLE IF EXISTS game_setup;
 CREATE TABLE game_setup
 (
-    id           SERIAL UNIQUE NOT NULL,
+    id           SERIAL,
     round_id     INTEGER,
     board_id     INTEGER,
-    expansion_id INTEGER,
-    CONSTRAINT fk_round_id
-        FOREIGN KEY (round_id)
-            REFERENCES rounds (id) ON DELETE CASCADE,
-    CONSTRAINT fk_board_id
-        FOREIGN KEY (board_id)
-            REFERENCES boards (id) ON DELETE CASCADE,
-    CONSTRAINT fk_expansion_id
-        FOREIGN KEY (expansion_id)
-            REFERENCES expansions (id) ON DELETE CASCADE
+    expansion_id INTEGER
 );
 
 DROP TABLE IF EXISTS points;
 CREATE TABLE points
 (
-    id                       SERIAL UNIQUE NOT NULL,
+    id                       SERIAL,
     round_id                 INTEGER       NOT NULL,
     player_id                INTEGER       NOT NULL,
     tr_number                INTEGER       NOT NULL,
@@ -196,13 +165,79 @@ CREATE TABLE points
     number_of_cities         INTEGER,
     greeneries_around_cities INTEGER,
     vp_on_cards              INTEGER,
-    sum_points               INTEGER,
-    CONSTRAINT fk_round_id
-        FOREIGN KEY (round_id)
-            REFERENCES rounds (id) ON DELETE CASCADE
+    sum_points               INTEGER
 );
 
+
+----------------------------------------------------Add Keys---------------------------------------------------------
+
+
+        --------------------------------------------Primary Keys-------------------------------------------------
+
+ALTER TABLE IF EXISTS ONLY public.images            ADD CONSTRAINT  pk_images_id            PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.corporations      ADD CONSTRAINT  pk_corporations_id      PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.boards            ADD CONSTRAINT  pk_boards_id            PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.expansions        ADD CONSTRAINT  pk_expansions_id        PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.milestones        ADD CONSTRAINT  pk_milestones_id        PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.awards            ADD CONSTRAINT  pk_awards_id            PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.players           ADD CONSTRAINT  pk_players_id           PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.leagues           ADD CONSTRAINT  pk_leagues_id           PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.league_players    ADD CONSTRAINT  pk_league_players       PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.rounds            ADD CONSTRAINT  pk_rounds_id            PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.round_players     ADD CONSTRAINT  pk_round_players_id     PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.game_setup        ADD CONSTRAINT  pk_game_setup_id        PRIMARY KEY (id);
+ALTER TABLE IF EXISTS ONLY public.points            ADD CONSTRAINT  pk_points_id            PRIMARY KEY (id);
+
+        --------------------------------------------Foreign Keys-------------------------------------------------
+
+ALTER TABLE IF EXISTS ONLY public.corporations      ADD CONSTRAINT
+    fk_expansion_id   FOREIGN KEY (expansion_id)    REFERENCES expansions (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.milestones        ADD CONSTRAINT
+    fk_board_id       FOREIGN KEY (board_id)        REFERENCES boards (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.milestones        ADD CONSTRAINT
+    fk_expansion_id   FOREIGN KEY (expansion_id)    REFERENCES expansions (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.awards            ADD CONSTRAINT
+    fk_board_id       FOREIGN KEY (board_id)        REFERENCES boards (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.awards            ADD CONSTRAINT
+    fk_expansion_id   FOREIGN KEY (expansion_id)    REFERENCES expansions (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.league_players    ADD CONSTRAINT
+    fk_league_id      FOREIGN KEY (league_id)       REFERENCES leagues (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.rounds            ADD CONSTRAINT
+    fk_league_id      FOREIGN KEY (league_id)       REFERENCES leagues (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.round_players     ADD CONSTRAINT
+    fk_round_id       FOREIGN KEY (round_id)        REFERENCES rounds (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.round_players     ADD CONSTRAINT
+    fk_corporation_id FOREIGN KEY (corporation_id)  REFERENCES corporations (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.game_setup        ADD CONSTRAINT
+    fk_round_id       FOREIGN KEY (round_id)        REFERENCES rounds (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.game_setup        ADD CONSTRAINT
+    fk_board_id       FOREIGN KEY (board_id)        REFERENCES boards (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.game_setup        ADD CONSTRAINT
+    fk_expansion_id   FOREIGN KEY (expansion_id)    REFERENCES expansions (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.points            ADD CONSTRAINT
+    fk_round_id       FOREIGN KEY (round_id)        REFERENCES rounds (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.points            ADD CONSTRAINT
+    fk_player_id      FOREIGN KEY (player_id)       REFERENCES round_players (id) ON DELETE CASCADE;
+
+
+
 ----------------------------------------------------Insert---------------------------------------------------------
+
+
+
 INSERT INTO
     boards(board_name)
 VALUES
@@ -258,35 +293,35 @@ VALUES
     ('Venuphille',      NULL,   2);
 
 INSERT INTO
-    corporations(name, expansion_id, image_id)
+    corporations(name, expansion_id)
 VALUES
-    ('Credicor',                        NULL, NULL),
-    ('Ecoline',                         NULL, NULL),
-    ('Helion',                          NULL, NULL),
-    ('Mining Guild',                    NULL, NULL),
-    ('Interplanetary Cinematics',       NULL, NULL),
-    ('Inventrix',                       NULL, NULL),
-    ('Phobolog',                        NULL, NULL),
-    ('Tharsis Rebuplic',                NULL, NULL),
-    ('Thorgate',                        NULL, NULL),
-    ('United Nations Mars Initiative',  NULL, NULL),
-    ('Teractor',                        NULL, NULL),
-    ('Saturn Systems',                  NULL, NULL),
-    ('Aphrodite',                       NULL, 2),
-    ('Celestic',                        NULL, 2),
-    ('Manutech',                        NULL, 2),
-    ('Morning Star Inc',                NULL, 2),
-    ('Viron',                           NULL, 2),
-    ('Cheung Shing Mars',               NULL, 1),
-    ('Point Luna',                      NULL, 1),
-    ('Robinson Industries',             NULL, 1),
-    ('Valley Trust',                    NULL, 1),
-    ('Vitor',                           NULL, 1),
-    ('Aridor',                          NULL, 3),
-    ('Arklight',                        NULL, 3),
-    ('Polyphemos',                      NULL, 3),
-    ('Poseidon',                        NULL, 3),
-    ('Storm Craft Incorporated',        NULL, 3);
+    ('Credicor',                        NULL),
+    ('Ecoline',                         NULL),
+    ('Helion',                          NULL),
+    ('Mining Guild',                    NULL),
+    ('Interplanetary Cinematics',       NULL),
+    ('Inventrix',                       NULL),
+    ('Phobolog',                        NULL),
+    ('Tharsis Rebuplic',                NULL),
+    ('Thorgate',                        NULL),
+    ('United Nations Mars Initiative',  NULL),
+    ('Teractor',                        NULL),
+    ('Saturn Systems',                  NULL),
+    ('Aphrodite',                       NULL),
+    ('Celestic',                        NULL),
+    ('Manutech',                        NULL),
+    ('Morning Star Inc',                NULL),
+    ('Viron',                           NULL),
+    ('Cheung Shing Mars',               NULL),
+    ('Point Luna',                      NULL),
+    ('Robinson Industries',             NULL),
+    ('Valley Trust',                    NULL),
+    ('Vitor',                           NULL),
+    ('Aridor',                          NULL),
+    ('Arklight',                        NULL),
+    ('Polyphemos',                      NULL),
+    ('Poseidon',                        NULL),
+    ('Storm Craft Incorporated',        NULL);
 
 
 /*Terraforming Mars database
