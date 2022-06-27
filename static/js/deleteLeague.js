18d663741a9140b0aa250ab7e64ede55ed992a0c
Zsezsu <zsezsu6@gmail.com>
2022-06-27 13:12:53 +0200

    Refactor deleteLeague.js

--- static/js/deleteLeague.js
+++ static/js/deleteLeague.js
@@ -2,18 +2,19 @@ import {dataHandler} from "./data/dataHandler.js";
 
 const deleteLeague = {
     init() {
+        console.log(window)
        this.clickOnDelete();
     },
 
-    clickOnDelete(){
-        let deleteButton = document.querySelector("#delete-button");
+    clickOnDelete() {
+        const deleteButton = document.querySelector("#delete-button");
         deleteButton.addEventListener('click', this.deleteLeague)
     },
 
-    async deleteLeague(e){
+    async deleteLeague(e) {
         if (window.confirm('Are you sure you want to delete this league?' +
                 '\n Be careful this is irreversible!')){
-            let leagueNumber = e.currentTarget.getAttribute('data-league-id');
+            const leagueNumber = e.currentTarget.getAttribute('data-league-id');
             await dataHandler.deleteLeague(leagueNumber);
             window.location.replace("/my-leagues");
         }
