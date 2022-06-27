7fb63cdcef1aef28608e65b9f789f874811d6883
benedekhalaj <bendihalaj@gmail.com>
2022-06-27 21:59:26 +0200

    Add ares-only corporations in ares_corporations.sql

---
+++ database/ares_corporations.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    corporations(name, expansion_id)
+VALUES
+    ('Celestior',                    NULL),
+    ('DevTechs',                     NULL),
+    ('Launch Star Incorporated',     NULL),
+    ('Mai-Ni Productions',           NULL),
+    ('Zetasel',                      NULL);
