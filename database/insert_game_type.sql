d1458fa6cbe862f7659efa812d90763ba2947fde
dudaskobende <dudaskobende@gmail.com>
2022-06-27 14:53:13 +0200

    Add new game_type_id foreing key column to expansions table.

--- database/insert_game_type.sql
+++ database/insert_game_type.sql
@@ -3,6 +3,9 @@
 ALTER TABLE IF EXISTS ONLY public.game_types_corporations_expansions
     DROP CONSTRAINT IF EXISTS fk_game_type_id CASCADE;
 
+ALTER TABLE IF EXISTS ONLY public.expansions
+    DROP CONSTRAINT IF EXISTS fk_game_type_id CASCADE;
+
 ALTER TABLE IF EXISTS ONLY public.game_types_corporations_expansions
     DROP CONSTRAINT IF EXISTS fk_expansion_id CASCADE;
 
@@ -40,6 +43,11 @@ CREATE TABLE game_types_corporations_expansions
 
 ALTER TABLE IF EXISTS corporations DROP COLUMN IF EXISTS expansion_id;
 
+
+-----------------------------------------------------------------------------------------------------------------
+-- ADD new column to expansions table.
+
+ALTER TABLE IF EXISTS expansions ADD COLUMN IF NOT EXISTS game_type_id INTEGER;
 ----------------------------------------------------Add Keys---------------------------------------------------------
 
 
@@ -62,3 +70,7 @@ ALTER TABLE IF EXISTS ONLY public.game_types_corporations_expansions
 ALTER TABLE IF EXISTS ONLY public.game_types_corporations_expansions
     ADD CONSTRAINT
         fk_corporation_id FOREIGN KEY (corporation_id) REFERENCES corporations (id) ON DELETE CASCADE;
+
+ALTER TABLE IF EXISTS ONLY public.expansions
+    ADD CONSTRAINT
+        fk_game_type_id FOREIGN KEY (game_type_id) REFERENCES game_types (id) ON DELETE CASCADE;
