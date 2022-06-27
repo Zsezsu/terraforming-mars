96afaa916de7b421c942a68b3601d2ce34946f1a
benedekhalaj <bendihalaj@gmail.com>
2022-06-27 22:35:26 +0200

    Add ares corporation relations to game_types_corporations_expansions table

--- database/ares_corporations.sql
+++ database/ares_corporations.sql
@@ -7,3 +7,25 @@ VALUES
     ('Mai-Ni Productions'),
     ('Zetasel');
 
+INSERT INTO
+    game_types_corporations_expansions(game_type_id, expansion_id, corporation_id)
+VALUES
+    (2, NULL, 1),
+    (2, NULL, 2),
+    (2, NULL, 3),
+    (2, NULL, 4),
+    (2, NULL, 5),
+    (2, NULL, 6),
+    (2, NULL, 7),
+    (2, NULL, 8),
+    (2, NULL, 9),
+    (2, NULL, 10),
+    (2, NULL, 11),
+    (2, NULL, 12),
+    (2, NULL, 24),
+    (2, NULL, 28),
+    (2, NULL, 29),
+    (2, NULL, 30),
+    (2, NULL, 31),
+    (2, NULL, 32);
+
