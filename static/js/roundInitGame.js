f98c29cf4d8406aa275a7765f3a91b0a666496e7
dudaskobende <dudaskobende@gmail.com>
2022-06-29 07:23:51 +0200

    Add hide expansions option from init round phase if game type has no expansions.

--- static/js/roundInitGame.js
+++ static/js/roundInitGame.js
@@ -1,6 +1,7 @@
 main();
 
 function main() {
+    toggleExpansionDiv();
     const showRoundFormButton = document.querySelector('.show-round-form-button');
     const closeRoundFormButton = document.querySelector('.close-round-form-button');
     [showRoundFormButton, closeRoundFormButton].forEach(
@@ -15,4 +16,14 @@ function toggleRoundForm() {
     const roundForm = document.querySelector('.round-form');
     roundForm.classList.toggle('hide-form-container');
     showRoundFormButton.disabled = !showRoundFormButton.disabled;
+}
+
+
+function toggleExpansionDiv() {
+    const expansions = document.querySelector('#expansions');
+    if (!hasExpansions(expansions)) {expansions.remove()}
+}
+
+function hasExpansions(expansions) {
+    return expansions.querySelector('input') != null;
 }
