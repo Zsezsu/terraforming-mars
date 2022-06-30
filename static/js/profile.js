7226ae777d8ff41bfee5e34300d20061e062453d
benedekhalaj <bendihalaj@gmail.com>
2022-06-30 17:49:41 +0200

    Add profile.js updateProfilePicture

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,1 +1,17 @@
-console.log("profile");
+import {dataHandler} from "./data/dataHandler.js";
+
+main();
+
+function main() {
+    const updateProfilePictureButton = document.querySelector('#update-profile-picture');
+    updateProfilePictureButton.addEventListener('click', updateProfilePicture);
+}
+
+async function updateProfilePicture() {
+    const userId = document.querySelector('#username').getAttribute('data-user-id');
+    const imageId = document.querySelector('#slideshow-profile').getAttribute('data-image-id');
+    const status = await dataHandler.updateProfilePicture(userId, imageId);
+    if (status === 200) {
+        alert('Successfully updated the profile picture!')
+    }
+}
