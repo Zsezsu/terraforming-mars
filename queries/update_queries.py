818ea315da71e7ef6915aa8ce8aa925bc9f63488
benedekhalaj <bendihalaj@gmail.com>
2022-06-30 17:48:27 +0200

    Add update_queries.update_profile_picture

--- queries/update_queries.py
+++ queries/update_queries.py
@@ -1,2 +1,15 @@
+from psycopg2.sql import SQL, Literal, Identifier
+
 from data_manager import execute_update
 
+
+def update_profile_picture(player_id, image_id):
+    query = """
+    UPDATE players
+    SET image_id = {image_id}
+    WHERE id = {player_id}
+    """
+    execute_update(SQL(query).format(
+        player_id=Literal(player_id),
+        image_id=Literal(image_id)
+    ))
