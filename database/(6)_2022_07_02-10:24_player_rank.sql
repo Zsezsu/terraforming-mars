1c1bca0beb9c0a5c2ddbec0e4d70bed24c1fb3d0
dudaskobende <dudaskobende@gmail.com>
2022-07-02 11:16:04 +0200

    Modify players table: add default player rank when add new player by set the default column value in psql

--- database/(6)_2022_07_02-10:24_player_rank.sql
+++ database/(6)_2022_07_02-10:24_player_rank.sql
@@ -1,3 +1,7 @@
+--DROP ranks id foreign key
+ALTER TABLE IF EXISTS ONLY public.players
+    DROP CONSTRAINT IF EXISTS fk_ranks_id;
+
 --------------------------------------------------CREATE ranks table--------------------------------------------------
 --DROP ranks id primary key
 ALTER TABLE IF EXISTS ONLY public.ranks
@@ -18,13 +22,21 @@ ALTER TABLE IF EXISTS ONLY public.ranks
     ADD CONSTRAINT pk_ranks_id PRIMARY KEY (id);
 
 -------------------------------------------------UPDATE players table-------------------------------------------------
---DROP ranks id foreign key
-ALTER TABLE IF EXISTS ONLY public.players
-    DROP CONSTRAINT IF EXISTS fk_ranks_id;
-
---ADD new ranks id column to players table
+--DROP ranks_id column if exist from players table
 ALTER TABLE IF EXISTS players
-    ADD COLUMN IF NOT EXISTS ranks_id INTEGER;
+    DROP COLUMN IF EXISTS ranks_id;
+--CREATE a function to get the default rank level from the table ranks with default rank level as an argument.
+CREATE OR REPLACE FUNCTION default_rank_level_id(default_rank_level INTEGER) RETURNS INT
+    LANGUAGE SQL AS
+$$
+SELECT id
+FROM ranks
+WHERE rank_level = default_rank_level;
+$$;
+--ADD new ranks id column to players table with default rank level
+ALTER TABLE IF EXISTS players
+    ADD COLUMN IF NOT EXISTS ranks_id INTEGER DEFAULT default_rank_level_id(1);
+
 
 --ADD ranks id foreign key to players table
 ALTER TABLE IF EXISTS public.players
