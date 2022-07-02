f25f9d92775237e4d7aaae0ae2fc88a23d984b58
dudaskobende <dudaskobende@gmail.com>
2022-07-02 12:33:18 +0200

    Add minimum points limit to ranks table

--- database/(6)_2022_07_02-10:24_player_rank.sql
+++ database/(6)_2022_07_02-10:24_player_rank.sql
@@ -11,9 +11,10 @@ ALTER TABLE IF EXISTS ONLY public.ranks
 DROP TABLE IF EXISTS ranks;
 CREATE TABLE public.ranks
 (
-    id         SERIAL,
-    name       TEXT,
-    rank_level INTEGER UNIQUE NOT NULL
+    id             SERIAL,
+    name           TEXT,
+    rank_level     INTEGER UNIQUE NOT NULL,
+    minimum_points INTEGER        NOT NULL
 
 );
 
@@ -42,16 +43,18 @@ ALTER TABLE IF EXISTS players
 ALTER TABLE IF EXISTS public.players
     ADD CONSTRAINT fk_ranks_id FOREIGN KEY (ranks_id) REFERENCES ranks (id);
 
+
+
 -----------------------------------------------------INSERT ranks-----------------------------------------------------
 
-INSERT INTO public.ranks(name, rank_level)
-VALUES ('Space Traveller', 1),
-       ('Dock Worker', 2),
-       ('Settler', 3),
-       ('trader', 4),
-       ('Astronaut', 5),
-       ('Builder', 6),
-       ('Terraformer', 7),
-       ('President', 8),
-       ('League Master', 9),
-       ('League Grand Master', 10);
+INSERT INTO public.ranks(name, rank_level, minimum_points)
+VALUES ('Space Traveller', 1, 0),
+       ('Dock Worker', 2, 3),
+       ('Settler', 3, 10),
+       ('trader', 4, 20),
+       ('Astronaut', 5, 30),
+       ('Builder', 6, 50),
+       ('Terraformer', 7, 80),
+       ('President', 8, 100),
+       ('League Master', 9, 120),
+       ('League Grand Master', 10, 150);
